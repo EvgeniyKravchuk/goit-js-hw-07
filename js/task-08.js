0f0e898e7bf335f71313b640000ab087887f5dd0
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-04-08 21:25:20 +0300

    Работаю на 8
    Осталось добавить создание боксов

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,22 @@
 const inputRef = document.querySelector('input');
+const boxesRef = document.querySelector('#boxes');
 const createButtonRef = document.querySelector("button[data-action='render']");
 const destroyButtonRef = document.querySelector("button[data-action='destroy']");
 
 const box = document.createElement('div');
 box.style.width = '30px';
 box.style.height = '30px';
-box.style.backgroundColor = randomColor;
 
-function randomColor() {
-    const color = `rgb(
-        ${Math.floor(Math.random() * 256},
-        $())`
-    return color
-}
+function makeRandomBackgroundColor(element) {
+    return element.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+};
+
+createButtonRef.addEventListener('click', createBoxes);
+destroyButtonRef.addEventListener('click', destroyBoxes)
+
+function createBoxes() {
+    
+};
+function destroyBoxes() {
+    boxesRef.innerHTML = '';
+};

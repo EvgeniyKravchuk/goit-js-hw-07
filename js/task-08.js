5bedc3d6aba09cf88bb3afea0abb928dfd459e69
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-04-08 22:43:01 +0300

    Доделал 8е

--- js/task-08.js
+++ js/task-08.js
@@ -3,20 +3,29 @@ const boxesRef = document.querySelector('#boxes');
 const createButtonRef = document.querySelector("button[data-action='render']");
 const destroyButtonRef = document.querySelector("button[data-action='destroy']");
 
-const box = document.createElement('div');
-box.style.width = '30px';
-box.style.height = '30px';
+let boxes = [];
 
 function makeRandomBackgroundColor(element) {
     return element.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
 };
 
-createButtonRef.addEventListener('click', createBoxes);
-destroyButtonRef.addEventListener('click', destroyBoxes)
+createButtonRef.addEventListener('click', onCreateBoxesClick);
+destroyButtonRef.addEventListener('click', onDestroyBoxesClick);
 
-function createBoxes() {
+function onCreateBoxesClick(ev) {
+    const value = inputRef.value;
     
+    for (let i = 0; i < value; i++) {
+        const box = document.createElement('div');
+        box.style.width = `${(30 + i * 10)}px`;
+        box.style.height = `${(30 + i * 10)}px`;
+        makeRandomBackgroundColor(box);
+        boxes.push(box)
+    }
+    boxesRef.prepend(...boxes)
+    boxes = []
 };
-function destroyBoxes() {
+function onDestroyBoxesClick (ev) {
     boxesRef.innerHTML = '';
 };
+
